a2b0ab861a56d8c816d53bbf8fc3da224f49f6be
Zhao KK <kikakkz@hotmail.com>
2022-01-24 12:47:05 +0000

    Update message version

--- pkg/crud/banapp/banapp.go
+++ pkg/crud/banapp/banapp.go
@@ -2,12 +2,14 @@ package banapp
 
 import (
 	"context"
+	"time"
 
 	constant "github.com/NpoolPlatform/appuser-manager/pkg/const"
 	npool "github.com/NpoolPlatform/message/npool/appusermgr"
 
 	db "github.com/NpoolPlatform/appuser-manager/pkg/db"
 	"github.com/NpoolPlatform/appuser-manager/pkg/db/ent"
+	"github.com/NpoolPlatform/appuser-manager/pkg/db/ent/banapp"
 
 	"github.com/google/uuid"
 
@@ -46,6 +48,7 @@ func Create(ctx context.Context, in *npool.CreateBanAppRequest) (*npool.CreateBa
 		BanApp.
 		Create().
 		SetAppID(uuid.MustParse(in.GetInfo().GetAppID())).
+		SetMessage(in.GetInfo().GetMessage()).
 		Save(ctx)
 	if err != nil {
 		return nil, xerrors.Errorf("fail create ban app: %v", err)
@@ -57,13 +60,107 @@ func Create(ctx context.Context, in *npool.CreateBanAppRequest) (*npool.CreateBa
 }
 
 func Get(ctx context.Context, in *npool.GetBanAppRequest) (*npool.GetBanAppResponse, error) {
-	return nil, nil
+	id, err := uuid.Parse(in.GetID())
+	if err != nil {
+		return nil, xerrors.Errorf("invalid id: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, constant.DBTimeout)
+	defer cancel()
+
+	cli, err := db.Client()
+	if err != nil {
+		return nil, xerrors.Errorf("fail get db client: %v", err)
+	}
+
+	infos, err := cli.
+		BanApp.
+		Query().
+		Where(
+			banapp.And(
+				banapp.ID(id),
+				banapp.DeleteAt(0),
+			),
+		).
+		All(ctx)
+	if err != nil {
+		return nil, xerrors.Errorf("fail query ban app: %v", err)
+	}
+
+	var banApp *npool.BanApp
+	for _, info := range infos {
+		banApp = dbRowToBanApp(info)
+		break
+	}
+
+	return &npool.GetBanAppResponse{
+		Info: banApp,
+	}, nil
 }
 
 func GetByApp(ctx context.Context, in *npool.GetBanAppByAppRequest) (*npool.GetBanAppByAppResponse, error) {
-	return nil, nil
+	ctx, cancel := context.WithTimeout(ctx, constant.DBTimeout)
+	defer cancel()
+
+	cli, err := db.Client()
+	if err != nil {
+		return nil, xerrors.Errorf("fail get db client: %v", err)
+	}
+
+	appID, err := uuid.Parse(in.GetAppID())
+	if err != nil {
+		return nil, xerrors.Errorf("invalid app id: %v", err)
+	}
+
+	infos, err := cli.
+		BanApp.
+		Query().
+		Where(
+			banapp.And(
+				banapp.AppID(appID),
+				banapp.DeleteAt(0),
+			),
+		).
+		All(ctx)
+	if err != nil {
+		return nil, xerrors.Errorf("fail query ban app by app: %v", err)
+	}
+
+	var banApp *npool.BanApp
+	for _, info := range infos {
+		banApp = dbRowToBanApp(info)
+		break
+	}
+
+	return &npool.GetBanAppByAppResponse{
+		Info: banApp,
+	}, nil
 }
 
 func Delete(ctx context.Context, in *npool.DeleteBanAppRequest) (*npool.DeleteBanAppResponse, error) {
-	return nil, nil
+	id, err := uuid.Parse(in.GetID())
+	if err != nil {
+		return nil, xerrors.Errorf("invalid id: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, constant.DBTimeout)
+	defer cancel()
+
+	cli, err := db.Client()
+	if err != nil {
+		return nil, xerrors.Errorf("fail get db client: %v", err)
+	}
+
+	info, err := cli.
+		BanApp.
+		UpdateOneID(id).
+		SetDeleteAt(uint32(time.Now().Unix())).
+		Save(ctx)
+	if err != nil {
+		return nil, xerrors.Errorf("fail delete ban app: %v", err)
+	}
+
+	return &npool.DeleteBanAppResponse{
+		Info: dbRowToBanApp(info),
+	}, nil
 }
