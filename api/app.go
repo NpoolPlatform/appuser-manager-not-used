c4bda2bdf537c554246682ffc92c9af82aef211b
Zhao KK <kikakkz@hotmail.com>
2022-01-24 06:51:30 +0000

    Add app apis

---
+++ api/app.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"context"
+
+	npool "github.com/NpoolPlatform/message/npool/appusermgr"
+)
+
+func CreateApp(ctx context.Context, in *npool.CreateAppRequest) (*npool.CreateAppResponse, error) {
+	return nil, nil
+}
+
+func UpdateApp(ctx context.Context, in *npool.UpdateAppRequest) (*npool.UpdateAppResponse, error) {
+	return nil, nil
+}
+
+func CreateAppControl(ctx context.Context, in *npool.CreateAppControlRequest) (*npool.CreateAppControlResponse, error) {
+	return nil, nil
+}
+
+func UpdateAppControl(ctx context.Context, in *npool.UpdateAppControlRequest) (*npool.UpdateAppControlResponse, error) {
+	return nil, nil
+}
+
+func CreateBanApp(ctx context.Context, in *npool.CreateBanAppRequest) (*npool.CreateBanAppResponse, error) {
+	return nil, nil
+}
+
+func DeleteBanApp(ctx context.Context, in *npool.DeleteBanAppRequest) (*npool.DeleteBanAppResponse, error) {
+	return nil, nil
+}
+
+func GetApp(ctx context.Context, in *npool.GetAppRequest) (*npool.GetAppResponse, error) {
+	return nil, nil
+}
+
+func GetApps(ctx context.Context, in *npool.GetAppsRequest) (*npool.GetAppsResponse, error) {
+	return nil, nil
+}
