4890230d614dfdf404e65457942e183afbc7c470
Zhao KK <kikakkz@hotmail.com>
2022-01-24 15:59:11 +0000

    Add get app role apis

--- api/role.go
+++ api/role.go
@@ -3,23 +3,49 @@ package api
 import (
 	"context"
 
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+
+	approlecrud "github.com/NpoolPlatform/appuser-manager/pkg/crud/approle"
 	npool "github.com/NpoolPlatform/message/npool/appusermgr"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *Server) CreateAppRole(ctx context.Context, in *npool.CreateAppRoleRequest) (*npool.CreateAppRoleResponse, error) {
-	return nil, nil
+	resp, err := approlecrud.Create(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorw("fail create app role: %v", err)
+		return &npool.CreateAppRoleResponse{}, status.Error(codes.Internal, err.Error())
+	}
+	return resp, nil
 }
 
 func (s *Server) GetAppRole(ctx context.Context, in *npool.GetAppRoleRequest) (*npool.GetAppRoleResponse, error) {
-	return nil, nil
+	resp, err := approlecrud.Get(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorw("fail get app role: %v", err)
+		return &npool.GetAppRoleResponse{}, status.Error(codes.Internal, err.Error())
+	}
+	return resp, nil
 }
 
 func (s *Server) GetAppRolesByApp(ctx context.Context, in *npool.GetAppRolesByAppRequest) (*npool.GetAppRolesByAppResponse, error) {
-	return nil, nil
+	resp, err := approlecrud.GetByApp(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorw("fail get app roles by app: %v", err)
+		return &npool.GetAppRolesByAppResponse{}, status.Error(codes.Internal, err.Error())
+	}
+	return resp, nil
 }
 
 func (s *Server) UpdateAppRole(ctx context.Context, in *npool.UpdateAppRoleRequest) (*npool.UpdateAppRoleResponse, error) {
-	return nil, nil
+	resp, err := approlecrud.Update(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorw("fail update app role: %v", err)
+		return &npool.UpdateAppRoleResponse{}, status.Error(codes.Internal, err.Error())
+	}
+	return resp, nil
 }
 
 func (s *Server) CreateAppRoleUser(ctx context.Context, in *npool.CreateAppRoleUserRequest) (*npool.CreateAppRoleUserResponse, error) {
