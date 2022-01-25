478af7bc93e259170d57e6f9442f68da5e232377
Zhao KK <kikakkz@hotmail.com>
2022-01-25 12:22:26 +0000

    For admin apps

--- pkg/middleware/admin/admin.go
+++ pkg/middleware/admin/admin.go
@@ -37,6 +37,7 @@ func CreateAdminApps(ctx context.Context, in *npool.CreateAdminAppsRequest) (*np
 
 	adminApp = npool.App{
 		ID:          constant.ChurchAppID,
+		CreatedBy:   "00000000-0000-0000-0000-000000000000",
 		Name:        constant.ChurchAppName,
 		Logo:        "NOT SET",
 		Description: "NOT SET",
