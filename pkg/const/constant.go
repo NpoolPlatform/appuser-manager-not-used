ae665381c193353002d639f703e27f94325a4490
fxnt <1330672764@qq.com>
2022-05-27 14:59:18 +0800

    update constant

--- pkg/const/constant.go
+++ pkg/const/constant.go
@@ -19,9 +19,4 @@ const (
 
 	ChurchAppName = "Church Dashboard"
 	ChurchAppID   = "ab4d1208-7da9-11ec-a6ea-fb41bda845cd"
-
-	ThirdGithub   = "github"
-	ThirdGoogle   = "google"
-	ThirdFaceBook = "facebook"
-	ThirdTwitter  = "twitter"
 )
