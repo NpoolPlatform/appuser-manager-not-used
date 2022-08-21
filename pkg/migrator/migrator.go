608e339d46a62f2794cc4f2ba699762e8b60aa69
fxnt <1330672764@qq.com>
2022-08-21 17:13:23 +0800

    update migrate

--- pkg/migrator/migrator.go
+++ pkg/migrator/migrator.go
@@ -2,6 +2,7 @@ package migrator
 
 import (
 	"context"
+
 	"github.com/NpoolPlatform/appuser-manager/pkg/db"
 	sm "github.com/NpoolPlatform/message/npool/appuser/mgr/v2/signmethod"
 )
@@ -15,22 +16,21 @@ func setSigninVerifyTypeVal(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+
 	appUserControls, err := cli.AppUserControl.Query().All(ctx)
 	if err != nil {
 		return err
 	}
 
 	for _, val := range appUserControls {
-		signinVerifyType := sm.SignMethodType_Email.String()
+		signinVerifyType := sm.SignMethodType_Google.String()
 		if val.SigninVerifyByGoogleAuthentication {
-			signinVerifyType = sm.SignMethodType_Google.String()
-		}
-
-		if _, err = cli.AppUserControl.
-			UpdateOneID(val.ID).
-			SetSigninVerifyType(signinVerifyType).
-			Save(ctx); err != nil {
-			return err
+			if _, err = cli.AppUserControl.
+				UpdateOneID(val.ID).
+				SetSigninVerifyType(signinVerifyType).
+				Save(ctx); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
