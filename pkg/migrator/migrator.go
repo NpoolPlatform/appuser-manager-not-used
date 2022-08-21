0cda737ea94c3163bea2c327331ae2bb13f7c9ec
fxnt <1330672764@qq.com>
2022-08-21 16:39:52 +0800

    delete admin migrate

--- pkg/migrator/migrator.go
+++ pkg/migrator/migrator.go
@@ -2,28 +2,11 @@ package migrator
 
 import (
 	"context"
-	"encoding/json"
-
-	"github.com/NpoolPlatform/go-service-framework/pkg/config"
-
 	"github.com/NpoolPlatform/appuser-manager/pkg/db"
-	"github.com/NpoolPlatform/appuser-manager/pkg/db/ent"
-	"github.com/NpoolPlatform/appuser-manager/pkg/db/ent/approle"
-
-	constant1 "github.com/NpoolPlatform/appuser-manager/pkg/const"
-	constant2 "github.com/NpoolPlatform/appuser-manager/pkg/message/const"
-	approlepb "github.com/NpoolPlatform/message/npool/appuser/mgr/v2/approle"
 	sm "github.com/NpoolPlatform/message/npool/appuser/mgr/v2/signmethod"
-
-	"github.com/google/uuid"
 )
 
 func Migrate(ctx context.Context) error {
-	err := updateAdminRoleAppID(ctx)
-	if err != nil {
-		return err
-	}
-
 	return setSigninVerifyTypeVal(ctx)
 }
 
@@ -46,49 +29,6 @@ func setSigninVerifyTypeVal(ctx context.Context) error {
 		if _, err = cli.AppUserControl.
 			UpdateOneID(val.ID).
 			SetSigninVerifyType(signinVerifyType).
-			Save(ctx); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func updateAdminRoleAppID(ctx context.Context) error {
-	hostname := config.GetStringValueWithNameSpace(constant2.ServiceName, config.KeyHostname)
-	genesisRoleStr := config.GetStringValueWithNameSpace(hostname, constant1.KeyGenesisRole)
-
-	appRoles := []*approlepb.AppRole{}
-
-	err := json.Unmarshal([]byte(genesisRoleStr), &appRoles)
-	if err != nil {
-		return err
-	}
-
-	cli, err := db.Client()
-	if err != nil {
-		return err
-	}
-
-	for _, val := range appRoles {
-		role, err := cli.
-			AppRole.
-			Query().
-			Where(
-				approle.Role(val.Role),
-			).
-			Only(ctx)
-		if err != nil {
-			if ent.IsNotFound(err) {
-				continue
-			}
-			return err
-		}
-
-		if _, err = role.
-			Update().
-			SetAppID(
-				uuid.MustParse(val.GetAppID()),
-			).
 			Save(ctx); err != nil {
 			return err
 		}
