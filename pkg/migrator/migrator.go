451f5d7ae2973d4be8be5e6785dcf59c55a5f83c
fxnt <1330672764@qq.com>
2022-08-31 14:49:27 +0800

    delete migration

--- pkg/migrator/migrator.go
+++ pkg/migrator/migrator.go
@@ -2,36 +2,8 @@ package migrator
 
 import (
 	"context"
-
-	"github.com/NpoolPlatform/appuser-manager/pkg/db"
-	sm "github.com/NpoolPlatform/message/npool/appuser/mgr/v2/signmethod"
 )
 
 func Migrate(ctx context.Context) error {
-	return setSigninVerifyTypeVal(ctx)
-}
-
-func setSigninVerifyTypeVal(ctx context.Context) error {
-	cli, err := db.Client()
-	if err != nil {
-		return err
-	}
-
-	appUserControls, err := cli.AppUserControl.Query().All(ctx)
-	if err != nil {
-		return err
-	}
-
-	for _, val := range appUserControls {
-		signinVerifyType := sm.SignMethodType_Google.String()
-		if val.SigninVerifyByGoogleAuthentication {
-			if _, err = cli.AppUserControl.
-				UpdateOneID(val.ID).
-				SetSigninVerifyType(signinVerifyType).
-				Save(ctx); err != nil {
-				return err
-			}
-		}
-	}
 	return nil
 }
